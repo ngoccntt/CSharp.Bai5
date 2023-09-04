e62e6e2efc9ca7cf7265ccdf41d1840cdd04bde8
ngoccntt <tranquocduong08012001@gmail.com>
2023-09-04 15:08:13 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Bai5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intA, intB, intC;
+            double ketqua;
+            Console.WriteLine(" ------------ CHUONG TRINH GIAI PHUONG TRINH BAC 1 ------------ ");
+            Console.WriteLine(" ------------------------- aX + b = 0 ------------------------- ");
+            Console.Write("Nhap a: ");
+
+        }
+    }
+}
