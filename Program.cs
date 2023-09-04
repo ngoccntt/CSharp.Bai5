e38e80733fc9b4a15aa29f32792dec54baaf5424
ngoccntt <tranquocduong08012001@gmail.com>
2023-09-04 15:49:10 +0700

    lan5

--- Program.cs
+++ Program.cs
@@ -11,11 +11,35 @@ namespace CSharp.Bai5
         static void Main(string[] args)
         {
             int intA, intB, intC;
+            
             double ketqua;
-            Console.WriteLine(" ------------ CHUONG TRINH GIAI PHUONG TRINH BAC 1 ------------ ");
-            Console.WriteLine(" ------------------------- aX + b = 0 ------------------------- ");
+            Console.WriteLine(" ------------ CHUONG TRINH GIAI PHUONG TRINH BAC 2 ------------ ");
+            Console.WriteLine(" ------------------------- ax^2 + bx + c =0 ------------------------- ");
             Console.Write("Nhap a: ");
-
+            intA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("{0}", "Nhap b: ");
+            intB = Convert.ToInt32(Console.ReadLine());
+            Console.Write("{0}", "Nhap c: ");
+            intC = Convert.ToInt32(Console.ReadLine());
+            
+            else
+            {
+                ketqua = (double)Math.Pow(2,3) - 4 * intA * intC;
+                Console.WriteLine(" Phuong trinh co 1 nghiem duy nhat x = {0}", ketqua);
+            }
+            Console.ReadKey();
         }
     }
 }
+if ()
+            {
+                Console.WriteLine(" Phuong trinh vo nghiem");
+            }
+            else if ()
+            {
+                Console.WriteLine(" Phuong trinh co nghiem kep");
+            }
+            else if ()
+            {
+                Console.WriteLine(" Phuong trinh co 2 nghiem phan biet");
+            }
